bd6dfc61fd15d80c59e1f3e188fb680ac8d19133
steve <stevestivv@gmail.com>
2022-12-24 04:07:27 -0600

    awesomewm config

---
+++ awesome/rc.lua
@@ -0,0 +1,42 @@
+-- If LuaRocks is installed, make sure that packages installed through it are
+-- found (e.g. lgi). If LuaRocks is not installed, do nothing.
+pcall(require, "luarocks.loader")
+
+-- Standard awesome library
+local gears = require("gears")
+local awful = require("awful")
+require("awful.autofocus")
+
+-- Widget and layout library
+local wibox = require("wibox")
+
+-- Theme handling library
+local beautiful = require("beautiful")
+
+-- {{{ Variable definitions
+-- This is used later as the default terminal and editor to run.
+terminal = "kitty"
+editor = os.getenv("EDITOR") or "editor"
+editor_cmd = terminal .. " -e " .. editor
+
+-- Default modkey.
+modkey = "Mod4"
+
+-- Themes define colours, icons, font and wallpapers.
+beautiful.init("~/.config/awesome/configs/theme.lua")
+
+-- Error handling
+require('configs.errors')
+
+-- menubar
+local menubar = require("menubar")
+require('configs.menubar').setup(awful, beautiful, gears, menubar, wibox)
+
+-- keybinds
+require('configs.keybinds').setup(awful, beautiful, gears, menubar, modkey)
+
+-- Rules
+require('configs.rules').setup(awful, beautiful, gears, modkey, wibox)
+
+-- focus
+require('configs.sloppyfocus').setup(beautiful)
