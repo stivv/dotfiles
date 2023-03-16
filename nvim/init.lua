4ad60052652c326a8aba6aa85b5a6a4ae139601a
steve <stevestivv@gmail.com>
2023-03-16 10:37:48 +0300

    switch to lazy.nvim updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,20 +1,11 @@
-require('options')
-require('keymaps')
-require('autocmds')
+require "config.options"
+require "config.lazy"
 
-
--- Lazy config
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    require "config.autocmds"
+    require "config.keymaps"
+    require "utils.contextmenu"
+  end,
+})
