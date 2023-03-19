373e31da2343a9c02803ef77e554c455f8c7fa77
steve <stevestivv@gmail.com>
2023-03-19 15:11:16 +0300

    lsp keymaps updates

--- nvim/lua/plugins/extras/lang/lua.lua
+++ nvim/lua/plugins/extras/lang/lua.lua
@@ -1,3 +1,8 @@
+-- Make runtime files discoverable to the server
+local runtime_path = vim.split(package.path, ";")
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
@@ -12,14 +17,25 @@ return {
 				lua_ls = {
 					settings = {
 						Lua = {
-							workspace = {
-								checkThirdParty = false,
-							},
 							completion = { callSnippet = "Replace" },
-							telemetry = { enable = false },
 							hint = {
 								enable = false,
 							},
+							runtime = {
+								-- Tell the language server which version of Lua you're using (most likely LuaJIT)
+								version = "LuaJIT",
+								-- Setup your lua path
+								path = runtime_path,
+							},
+							diagnostics = {
+								globals = { "vim" },
+							},
+							workspace = {
+								library = vim.api.nvim_get_runtime_file("", true),
+								checkThirdParty = false,
+							},
+							-- Do not send telemetry data containing a randomized but unique identifier
+							telemetry = { enable = false },
 						},
 					},
 				},
--- nvim/lua/plugins/lsp/servers.lua
+++ nvim/lua/plugins/lsp/servers.lua
@@ -26,6 +26,11 @@ function M.setup(_, opts)
 			require("lspconfig")[server].setup(server_opts)
 		end,
 	})
+
+	vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
+	vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
+	vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
+	vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
 end
 
 return M
