f8f96cbea2f87d73e8a23ea0fd9c367ae1cb9a8b
steve <stevestivv@gmail.com>
2023-03-22 19:44:10 +0300

    updates

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,13 +1,7 @@
 -- [[ Basic Keymaps ]]
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-
--- Keymaps
 local noremap = { noremap = true, silent = true }
 
 -- Keymaps for better default experience
--- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
