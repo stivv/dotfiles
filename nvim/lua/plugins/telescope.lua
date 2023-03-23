3aee4e5eb88262ff5c6d8ebf683eb8138e78793b
steve <stevestivv@gmail.com>
2023-03-23 11:40:35 +0300

    telescope ignore pattern add folders

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -35,7 +35,7 @@ return {
 						['<C-Up>'] = actions.cycle_history_prev,
 					},
 				},
-				file_ignore_patterns = { '.git/' },
+				file_ignore_patterns = { '.git/', 'node_modules', 'public' },
 			},
 			pickers = {
 				find_files = {
