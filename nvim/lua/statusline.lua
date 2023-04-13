df377cfa89aed32ede4427524202b043a0c0583a
steve <stevestivv@gmail.com>
2023-04-13 13:52:04 +0300

    statusline lsp diagnostics display changes

--- nvim/lua/statusline.lua
+++ nvim/lua/statusline.lua
@@ -6,46 +6,54 @@ hi Status4 gui=bold guifg=#fb3333 guibg=NONE
 ]])
 
 vim.g.status = {
-	n = "Normal",
-	no = "Normal·Operator Pending",
-	v = "Visual",
-	V = "V·Line",
-	-- '^V' = 'V·Block',
-	s = "Select",
-	S = "S·Line",
-	-- ^S = 'S·Block',
-	i = "Insert",
-	R = "Replace",
-	Rv = "V·Replace",
-	c = "Command",
-	cv = "Vim Ex",
-	ce = "Ex",
-	r = "Prompt",
-	rm = "More",
-	-- r? = 'Confirm',
-	-- ! = 'Shell',
-	t = "Terminal",
+	["n"] = "Normal",
+	["no"] = "Normal·Operator Pending",
+	["v"] = "Visual",
+	["V"] = "V·Line",
+	[""] = "V·Block",
+	["s"] = "Select",
+	["S"] = "S·Line",
+	[""] = "S·Block",
+	["i"] = "Insert",
+	["R"] = "Replace",
+	["Rv"] = "V·Replace",
+	["c"] = "Command",
+	["cv"] = "Vim Ex",
+	["ce"] = "Ex",
+	["r"] = "Prompt",
+	["rm"] = "More",
+	["r?"] = "Confirm",
+	["!"] = "Shell",
+	["t"] = "Terminal",
 }
 
+vim.g.diagnostics = function(type)
+	return vim.tbl_count(vim.diagnostic.get(0, { severity = vim.diagnostic.severity[type] }))
+end
+
 local function status_line()
-	local mode = "%#Status2# %-5{%g:status[v:lua.vim.fn.mode()]%} "
+	local mode = "%#Status2# %-5{%g:status[v:lua.vim.fn.mode()]%}  "
 	local file_name = "%#Status1#  %-.16t"
-	local modified = " %-m"
-	local branch = " %#Status3# " .. vim.fn.system("git -C . branch --show-current")
+	local modified = " %#Status4#%M"
+	local branch = "  %#Status3# " .. vim.fn.system("git -C . branch --show-current")
 	local right_align = "%#Status1#%="
-	local diagnostics = "%#Status4#  %{%v:lua.vim.tbl_count(v:lua.vim.diagnostic.get())%}"
+	local hint = "%#Status2#  %{%g:diagnostics('HINT')%}"
+	local warn = "%#Status3#  %{%g:diagnostics('WARN')%}"
+	local error = "%#Status4#  %{%g:diagnostics('ERROR')%}"
 	local file_type = "   %#Status1# %Y"
 	local line_no = "   %#Status3# %(%l/%L%)"
 	local pct_thru_file = "   %#Status2#%2p%%  "
 
 	return string.format(
-		"%s%s%s%s%s%s%s%s%s",
+		"%s%s%s%s%s%s%s%s%s%s%s",
 		mode,
 		file_name,
-		modified,
+		modified:gsub("+", "s"),
 		branch:gsub("\n", ""),
 		right_align,
-		diagnostics,
+		hint,
+		warn,
+		error,
 		file_type,
 		line_no,
 		pct_thru_file
