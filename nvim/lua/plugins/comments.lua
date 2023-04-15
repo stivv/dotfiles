12dbf53340f6c566c4d988eafeecbbe6cc7ae225
steve <stevestivv@gmail.com>
2023-04-15 10:46:06 +0300

    config refactor

--- nvim/lua/plugins/comments.lua
+++ nvim/lua/plugins/comments.lua
@@ -1,12 +1,5 @@
 return {
-	'numToStr/Comment.nvim',
-	event = 'BufReadPre',
-	config = function()
-		-- Enable Comment.nvim
-		local ok, comment = pcall(require, 'Comment')
-
-		if not ok then return end
-
-		comment.setup()
-	end
+	"numToStr/Comment.nvim",
+	event = "BufReadPre",
+	config = true,
 }
