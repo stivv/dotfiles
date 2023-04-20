092bfea714b8aaf1a11ad5a7bcbce88e55cd45bc
steve <stevestivv@gmail.com>
2023-04-20 18:42:42 +0300

    c lsp setup

---
+++ nvim/lua/plugins/lsp/servers/c.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, { "html" })
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				clangd = { settings = {} },
+			},
+			setup = {},
+		},
+	},
+}
